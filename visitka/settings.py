03b74d050430ede4a10c17b4cff20419b306ac0d
AlexMelnik2003 <alexz.melnik.02@gmail.com>
2024-07-05 17:23:47 +0600

    11

--- visitka/settings.py
+++ visitka/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,8 +24,16 @@ SECRET_KEY = 'django-insecure-h@4l&6h52htg%rmz5^h+%%g5xy!7lp!%^%pky^rq+!4lj@09_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['andrey.tour-aktau-kazakhstan.kz',
+                 'www.andrey.tour-aktau-kazakhstan.kz',
+                 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://andrey.tour-aktau-kazakhstan.kz',
+                        'https://www.andrey.tour-aktau-kazakhstan.kz/',
+                        ]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -70,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'visitka.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
